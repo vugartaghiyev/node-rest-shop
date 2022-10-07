0c5055cde1a6c88e073ea80c4bff98a6585bbb60
vugartaghiyev <vugartaghiye00@gmail.com>
2022-10-07 16:38:06 +0400

    Body and handling CORS

--- api/routes/products.js
+++ api/routes/products.js
@@ -8,7 +8,13 @@ router.get("/", (req, res, next) => {
 });
 
 router.post("/", (req, res, next) => {
+  const product = {
+    name: req.body.name,
+    price: req.body.price,
+  };
+
   res.status(201).json({
+    createdProd: product,
     message: "Handling POST requests to /products",
   });
 });
--- app.js
+++ app.js
@@ -13,6 +13,17 @@ app.use(
     extended: false,
   })
 );
+app.use(bodyParser.json());
+
+app.use((res, req, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  if (req.method == "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "*");
+    return res.status(200).json({});
+  }
+  next();
+});
 
 app.use("/products", productsRouter);
 app.use("/orders", ordersRouter);
