15c86cdd2db536cde95f29ab1302c4054fd7c29f
vugartaghiyev <vugartaghiyev00@gmail.com>
2022-10-12 10:24:06 -0400

    MongoDB and Mongoose part 2

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,24 +1,33 @@
 import express from "express";
 const router = express.Router();
 
-import {Product} from '../models/product.js'
+import { Product } from "../models/product.js";
 
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "Handling GET requests to /products",
-  }); 
+  Product.find()
+    .exec()
+    .then((data) => {
+      console.log(data);
+      res.status(200).json(data);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        error,
+      });
+    });
 });
 
 router.post("/", (req, res, next) => {
   const product = new Product({
     _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
-    price: req.body.price
-  })
+    price: req.body.price,
+  });
 
-  product.save().then(console.log)
+  product.save().then(console.log);
 
   res.status(201).json({
     createdProd: product,
@@ -28,27 +37,61 @@ router.post("/", (req, res, next) => {
 
 router.get("/:id", (req, res, next) => {
   const id = req.params.id;
-  Product.findById(id).exec().then((data) => {
-    res.status(200).json(data)
-  }).catch((error) => {
-    res.status(500).json({error})
-  })
+  Product.findById(id)
+    .exec()
+    .then((data) => {
+      if (data) res.status(200).json(data);
+      else
+        res.status(404).json({
+          message: "No valid entry found for provided id.",
+        });
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
 });
 
 router.patch("/:id", (req, res, next) => {
   const id = req.params.id;
 
-  res.status(200).json({
-    message: "Updated " + id,
-  });
+  // update is not working
+  Product.updateMany(
+    { _id: id },
+    {
+      $set: {
+        name: req.body.name,
+        price: req.body.price,
+      },
+    }
+  )
+    .exac()
+    .then((data) => {
+      console.log(data);
+      res.status(200).json(data);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        error,
+      });
+    });
 });
 
 router.delete("/:id", (req, res, next) => {
   const id = req.params.id;
 
-  res.status(200).json({
-    message: "Deleted " + id,
-  });
+  Product.remove({
+    _id: id,
+  })
+    .exec()
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((error) => {
+      res.status(500).json({
+        error,
+      });
+    });
 });
 
 export default router;
